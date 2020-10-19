81fa2f5d0464cd06bea47ce47af64510acb6a717
berlianafd <berlianafd@gmail.com>
2020-10-19 08:15:55 +0700

    Initial Commit

---
+++ Assets/Scripts/HalamanUtama.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HalamanUtama : MonoBehaviour
+{
+ 	public void pindahHalaman(string halaman){
+ 		SceneManager.LoadScene(halaman);
+ 	}   
+
+ 	public void keluarAplikasi(){
+ 		Application.Quit();
+ 	}
+}
---
+++ Assets/Scripts/TangkapObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TangkapObject : MonoBehaviour
+{
+	public GameObject bola;
+	public GameObject tangan;
+	public GameObject player;
+
+	bool tertangkap = false;
+	Vector3 posisiBola;
+    // Start is called before the first frame update
+    void Start()
+    {
+     	posisiBola = bola.transform.position;   
+     	player.transform.position = new Vector3(0f,1f, 0f);
+     	player.transform.eulerAngles = new Vector3(
+     		player.transform.eulerAngles.x,
+		    player.transform.eulerAngles.y + 44,
+		    player.transform.eulerAngles.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+        	if(!tertangkap){
+        		bola.GetComponent<Renderer>().material.color = Color.blue;        		
+        		bola.transform.SetParent(tangan.transform);
+        		bola.transform.localPosition = new Vector3(0.17f,-0.672f, 0f);
+        		tertangkap=true;
+        	} else if(tertangkap){
+        		bola.GetComponent<Renderer>().material.color = Color.red;        		
+        		bola.transform.SetParent(null);
+        		bola.transform.localPosition =  posisiBola;
+        		tertangkap=false;
+        	}
+        	
+        }
+    }
+}
